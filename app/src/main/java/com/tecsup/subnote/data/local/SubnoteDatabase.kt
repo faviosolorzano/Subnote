1ae1bcafbb3f98248bc18a678a011fecf21f77cb
favio <favio.solorzano@tecsup.edu.pe>
2026-06-17 16:50:58 -0500

    feat: agregar SubnoteDatabase con Room

---
+++ app/src/main/java/com/tecsup/subnote/data/local/SubnoteDatabase.kt
@@ -0,0 +1,29 @@
+package com.tecsup.subnote.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Suscripcion::class], version = 1, exportSchema = false)
+abstract class SubnoteDatabase : RoomDatabase() {
+
+    abstract fun suscripcionDao(): SuscripcionDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SubnoteDatabase? = null
+
+        fun getDatabase(context: Context): SubnoteDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instancia = Room.databaseBuilder(
+                    context.applicationContext,
+                    SubnoteDatabase::class.java,
+                    "subnote_database"
+                ).build()
+                INSTANCE = instancia
+                instancia
+            }
+        }
+    }
+}
